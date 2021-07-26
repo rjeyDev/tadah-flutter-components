9efee8cdc85cd3be0db2af66b6ca1530f55cd1c8
Rasul Jumaniyazov <rasuljumaniyazovv@gmail.com>
2021-07-27 00:21:37 +0500

    rounded tabbar

--- lib/components/selection_controls/tabbar.dart
+++ lib/components/selection_controls/tabbar.dart
@@ -2,10 +2,11 @@
 import 'package:flutter/material.dart';
 import 'package:tadah_flutter_components/tadah_flutter_components.dart';
 
-class AppTabBar extends StatefulWidget {
-  AppTabBar(
+class UnderlinedTabBar extends StatefulWidget {
+  UnderlinedTabBar(
       {Key key,
       @required this.tabController,
+      this.prefix,
       this.focusNode,
       this.autofocus = false,
       this.onTap,
@@ -15,6 +16,7 @@ class AppTabBar extends StatefulWidget {
       : super(key: key);
 
   final FocusNode focusNode;
+  final Widget prefix;
   final bool autofocus;
   final TabController tabController;
   final bool isScrollable;
@@ -23,11 +25,12 @@ class AppTabBar extends StatefulWidget {
   final List<Widget> tabs;
 
   @override
-  _AppTabBarState createState() => _AppTabBarState();
+  _UnderlinedTabBarState createState() => _UnderlinedTabBarState();
 }
 
-class _AppTabBarState extends State<AppTabBar> {
+class _UnderlinedTabBarState extends State<UnderlinedTabBar> {
   bool focused = false;
+  bool get hasPrefix => widget.prefix != null;
 
   @override
   void initState() {
@@ -41,6 +44,7 @@ class _AppTabBarState extends State<AppTabBar> {
       data: ThemeData(
         splashColor: AppColors.TRANSPARENT,
         highlightColor: AppColors.TRANSPARENT,
+        hoverColor: AppColors.TRANSPARENT,
       ),
       child: Focus(
         focusNode: widget.focusNode,
@@ -55,7 +59,9 @@ class _AppTabBarState extends State<AppTabBar> {
           child: TabBar(
             controller: widget.tabController,
             isScrollable: widget.isScrollable ?? true,
-            labelPadding: EdgeInsets.all(8),
+            labelPadding: hasPrefix
+                ? EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+                : EdgeInsets.all(8),
             labelColor: widget.disabled
                 ? AppColors.TEXT_DISABLED_LIGHT
                 : AppColors.ACCENT_MAIN,
@@ -87,7 +93,125 @@ class _AppTabBarState extends State<AppTabBar> {
                     ),
                   ),
             onTap: widget.onTap,
-            tabs: widget.tabs,
+            tabs: hasPrefix
+                ? widget.tabs
+                    .map((e) => Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            widget.prefix,
+                            SizedBox(width: 10),
+                            e,
+                          ],
+                        ))
+                    .toList()
+                : widget.tabs,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedTabBar extends StatefulWidget {
+  RoundedTabBar(
+      {Key key,
+      @required this.tabController,
+      this.prefix,
+      this.focusNode,
+      this.autofocus = false,
+      this.onTap,
+      this.disabled = false,
+      this.isScrollable,
+      this.tabs})
+      : super(key: key);
+
+  final FocusNode focusNode;
+  final Widget prefix;
+  final bool autofocus;
+  final TabController tabController;
+  final bool isScrollable;
+  final bool disabled;
+  final Function(int) onTap;
+  final List<Widget> tabs;
+
+  @override
+  _RoundedTabBarState createState() => _RoundedTabBarState();
+}
+
+class _RoundedTabBarState extends State<RoundedTabBar> {
+  bool focused = false;
+  bool pressed = false;
+  bool get hasPrefix => widget.prefix != null;
+
+  @override
+  void initState() {
+    super.initState();
+    focused = widget.autofocus;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        splashColor: AppColors.TRANSPARENT,
+        highlightColor: AppColors.TRANSPARENT,
+        hoverColor: AppColors.TRANSPARENT,
+      ),
+      child: Focus(
+        focusNode: widget.focusNode,
+        autofocus: widget.autofocus,
+        onFocusChange: (focus) {
+          setState(() {
+            focused = focus;
+          });
+        },
+        child: IgnorePointer(
+          ignoring: widget.disabled,
+          child: TabBar(
+            controller: widget.tabController,
+            isScrollable: widget.isScrollable ?? true,
+            labelPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            labelColor: widget.disabled
+                ? AppColors.TEXT_DISABLED_LIGHT
+                : AppColors.ACCENT_MAIN,
+            labelStyle: AppTextStyles.SECONDARY,
+            unselectedLabelStyle: AppTextStyles.SECONDARY,
+            unselectedLabelColor: widget.disabled
+                ? AppColors.TEXT_DISABLED_LIGHT
+                : AppColors.COOL_GRAY_500,
+            indicator: widget.disabled
+                ? BoxDecoration()
+                : BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: pressed
+                        ? AppColors.BLUE_VIOLET_500_24_WO
+                        : AppColors.BLUE_VIOLET_500_8_WO,
+                    border: Border.all(
+                      color: focused
+                          ? AppColors.ACCENT_MAIN
+                          : AppColors.TRANSPARENT,
+                      width: 1.5,
+                    ),
+                  ),
+            onTap: (index) {
+              if (focused)
+                setState(() {
+                  focused = false;
+                });
+              widget.onTap(index);
+            },
+            tabs: hasPrefix
+                ? widget.tabs
+                    .map((e) => Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            widget.prefix,
+                            SizedBox(width: 10),
+                            e,
+                          ],
+                        ))
+                    .toList()
+                : widget.tabs,
           ),
         ),
       ),
